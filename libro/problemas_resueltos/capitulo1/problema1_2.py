2132573a35b09bbe12d443067e7c557d9628504a
JaimeOmar1904 <jaimemarcial@comunidad.unam.mx>
2019-09-19 17:40:25 -0500

    ejercicios en clase

---
+++ libro/problemas_resueltos/capitulo1/problema1_2.py
@@ -0,0 +1,4 @@
+BASE = float(input("Dame la base del triangulo"))
+ALTU = float(input("Dame la altura del triangulo"))
+SUP = ( BASE * ALTU)/2
+print(f"El area del trinagulo es {SUP} y la base del trinagulo es de {BASE} y la latura es: {ALTU} ")
---
+++ libro/problemas_resueltos/capitulo1/problema1_3.py
@@ -0,0 +1,7 @@
+NOM = str(input("Escribe el nombre del dinosaurio"))
+PES = float(input("Escribe el peso del dinosaurio"))
+LON = float(input("Escribe la longitud del dinosaurio"))
+PESKIL = PES * 1000
+LONMET = LON * 0.3047
+print(f"El nombre del dinosaurio es {NOM} el peso es de {Peso} y su peso en kilos {PESKIL} y su longitud de {LONMET}")
+
