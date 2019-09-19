96c6ae3e18b5a1afb2174e850cd98a7655d2eb8a
JaimeOmar1904 <54325865+JaimeOmar1904@users.noreply.github.com>
2019-09-19 17:42:16 -0500

    Add files via upload

---
+++ ejemmplo1_10.py
@@ -0,0 +1,7 @@
+BASE = int ( input (" Escribe la base del triangulo: " ))
+ALTU = int ( input (" Escribe la altura del triangulo: " ))
+SUP = BASE * ALTU
+PER = 2*(BASE + ALTU)
+print(f"La base del triangulo {BASE} y la altura {ALTU} es {SUP} ")
+print(f"La base del triangulo {BASE} y la altura {ALTU} es {PER} ")
+
