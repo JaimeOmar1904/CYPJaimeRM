e1dc84c683e30aea6f0672c75e7505e884a4f5fe
JaimeOmar1904 <jaimemarcial@comunidad.unam.mx>
2019-09-22 17:39:18 -0500

    Ejemplos terminados del libro

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,7 @@
+CAL1 = int(input("Escribe la primera calificación: "))
+CAL2 = int(input("Escribe la segunda calificación: "))
+CAL3 = int(input("Escribe la tercera calificación: "))
+CAL4 = int(input("Escribe la cuarta calificación: "))
+CAL5 = int(input("Escribe la quinta calificación: "))
+PRO =(CAL1 + CAL2 + CAL3+ CAL4 + CAL5)/5
+print(f"Las calificaciónes son: {CAL1}, {CAL2}, {CAL3}, {CAL4} y {CAL5} y tu calificación final es: {PRO}")
---
+++ libro/ejmeplo1_15.py
@@ -0,0 +1,6 @@
+BASE = float(input("Escibe la base del rectángulo: "))
+ALTU = float(input("Escribe la altura de rectángulo: "))
+SUP = BASE * ALTU
+PER = 2*(BASE + ALTU)
+print(f"La base del rectángulo es {BASE} Y la latura es {ALTU} y su superfice es {SUP}")
+print(f"La base del rectángulo es {BASE} Y la latura es {ALTU} y su perimetro es {PER}")
