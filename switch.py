00c7f4b73b7d7ead0ca57c0ba4f4531432fc49d9
JaimeOmar1904 <jaimemarcial@comunidad.unam.mx>
2019-10-08 17:47:49 -0500

    trabajo

---
+++ switch.py
@@ -0,0 +1,17 @@
+dia = int(input("indroduce un dia (1-5)"))
+if dia == 1 :
+    print("lunes")
+elif dia == 2:
+    print("martes")
+elif dia == 3:
+    print("miercoles")
+elif dia == 4:
+    print("jueves")
+elif dia == 5:
+    print("viernes")
+else:
+    print("opcion no válida")
+
+print(f"fin del programa!!")
+
+
